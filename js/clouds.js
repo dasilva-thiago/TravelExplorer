a0ff46cbc71172643b31cc8428a905660f16e217
dasilva-thiago <thiagosilva785@gmail.com>
2026-05-11 21:45:50 -0300

    feat(hero): implement Vanta.js clouds background and update hero section layout

---
+++ js/clouds.js
@@ -0,0 +1,12 @@
+VANTA.CLOUDS({
+    el: "#vanta-hero",
+    mouseControls: true,  
+    touchControls: true,
+    gyroControls: false,
+    speed: 1.2,
+    skyColor: 0x5482a9,     
+    cloudColor: 0xffffff,
+    sunColor: 0xe8b428,    
+    sunGlareColor: 0xe8b428,
+    sunlightColor: 0xd4eeff,
+})
